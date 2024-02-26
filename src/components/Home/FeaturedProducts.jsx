82f03d0f6b2f4d1a3a40bf63e67ec8de1f1c3d57
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-02-25 19:03:40 -0500

    Creating Product Card

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,4 +1,5 @@
 import "./FeaturedProducts.css";
+import ProductCard from "./ProductCard";
 
 const FeaturedProducts = () => {
   return (
@@ -6,7 +7,9 @@ const FeaturedProducts = () => {
       <h2>Featured Products</h2>
 
       <div className="align_center featured_products_list">
-        <article className="product_card">Product</article>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   );
