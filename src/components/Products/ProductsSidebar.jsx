d2cd715b21f80cab7c521a736735e4ab52682533
Siddhant <technosys.sidd@gmail.com>
2024-02-26 13:46:02 -0500

    Building Product Page

---
+++ src/components/Products/ProductsSidebar.jsx
@@ -0,0 +1,20 @@
+import LinkWithIcon from "../Navbar/LinkWithIcon";
+import "./ProductsSidebar.css";
+import rocket from "../../assets/rocket.png";
+
+const ProductsSidebar = () => {
+  return (
+    <aside className="products_sidebar">
+      <h2>Category</h2>
+      <div className="category_links">
+        <LinkWithIcon
+          title="Electronics"
+          link="products?category=electronics"
+          emoji={rocket}
+          sidebar={true}
+        />
+      </div>
+    </aside>
+  );
+};
+export default ProductsSidebar;
