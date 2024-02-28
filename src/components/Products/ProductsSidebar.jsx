015b8dc5df04d3de58ba0eab3b76206fc54bc3f1
Siddhant <technosys.sidd@gmail.com>
2024-02-28 17:54:53 -0500

    Creating Fetching Custom Hook

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,33 +1,24 @@
-import apiClient from "../../utils/api-client";
+import useData from "../../hooks/useData";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import "./ProductsSidebar.css";
-import rocket from "../../assets/rocket.png";
-import { useEffect, useState } from "react";
 
 const ProductsSidebar = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get("/category")
-      .then((res) => setCategories(res.data))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { data: categories, error } = useData("/category");
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
       <div className="category_links">
         {error && <em className="form_error">{error}</em>}
-        {categories.map((category) => (
-          <LinkWithIcon
-            key={category._id}
-            title={category.name}
-            link={`products?category=${category.name}`}
-            emoji={`http://localhost:8000/category/${category.image}`}
-            sidebar={true}
-          />
-        ))}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              key={category._id}
+              title={category.name}
+              link={`products?category=${category.name}`}
+              emoji={`http://localhost:8000/category/${category.image}`}
+              sidebar={true}
+            />
+          ))}
       </div>
     </aside>
   );
---
+++ src/hooks/useData.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import apiClient from "../utils/api-client";
+
+const useData = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get(url)
+      .then((res) => setData(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+  return { data, error };
+};
+export default useData;
