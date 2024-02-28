97b9d0dfac72a6d9398a23efad86329e47139fe4
Siddhant <technosys.sidd@gmail.com>
2024-02-28 17:33:27 -0500

    Fetching Categories

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,18 +1,33 @@
+import apiClient from "../../utils/api-client";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 import "./ProductsSidebar.css";
 import rocket from "../../assets/rocket.png";
+import { useEffect, useState } from "react";
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      .then((res) => setCategories(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
       <div className="category_links">
-        <LinkWithIcon
-          title="Electronics"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories.map((category) => (
+          <LinkWithIcon
+            key={category._id}
+            title={category.name}
+            link={`products?category=${category.name}`}
+            emoji={`http://localhost:8000/category/${category.image}`}
+            sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
