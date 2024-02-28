b5b863df55a816bfeff76fd1c5ae26859f73c8b7
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-02-27 21:49:50 -0500

    Implementing Routing in our Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import ProductsPage from "./components/Products/ProductsPage";
-import HomePage from "./components/Home/HomePage";
-import SingleProductPage from "./components/SingleProduct/SingleProductPage";
-import CartPage from "./components/Cart/CartPage";
-import MyOrderPage from "./components/MyOrder/MyOrderPage";
-import LoginPage from "./components/Authentication/LoginPage";
-import "./components/Authentication/SignupPage.css";
-import SignupPage from "./components/Authentication/SignupPage";
+import Routing from "./components/Routing/Routing";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <main>
-        {/* <HomePage /> */}
-        {/* <ProductsPage /> */}
-        {/* <SingleProductPage /> */}
-        {/* <CartPage /> */}
-        {/* <MyOrderPage /> */}
-        {/* <LoginPage /> */}
-        <SignupPage />
+        <Routing />
       </main>
     </div>
   );
---
+++ src/components/Routing/Routing.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import ProductsPage from "../Products/ProductsPage";
+import HomePage from "../Home/HomePage";
+import SingleProductPage from "../SingleProduct/SingleProductPage";
+import CartPage from "../Cart/CartPage";
+import MyOrderPage from "../MyOrder/MyOrderPage";
+import LoginPage from "../Authentication/LoginPage";
+import "../Authentication/SignupPage.css";
+import SignupPage from "../Authentication/SignupPage";
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/1" element={<SingleProductPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/myorders" element={<MyOrderPage />} />
+    </Routes>
+  );
+};
+export default Routing;
