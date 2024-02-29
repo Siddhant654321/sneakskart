c845851179d4fed27ef2f744f8d76e28bd541ca9
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-02-28 21:42:51 -0500

    Infinite Scrolling in React

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -5,28 +5,54 @@ import "./ProductCard.css";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 import "./ProductsList.css";
 import Pagination from "../Common/Pagination";
+import { useEffect, useState } from "react";
 
 const ProductsList = () => {
+  const [page, setPage] = useState(1);
   const [search, setSearch] = useSearchParams();
   const category = search.get("category");
-  const page = search.get("page");
   const { data, error, isLoading } = useData(
     "/products",
     {
       params: {
         category,
+        perPage: 10,
         page,
       },
     },
     [category, page],
   );
+
+  useEffect(() => {
+    setPage(1);
+  }, [category]);
+
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   const handlePageChange = (page) => {
     const currentParams = Object.fromEntries([...search]);
 
-    setSearch({ ...currentParams, page: page });
+    setSearch({ ...currentParams, page: parseInt(currentParams.page) + 1 });
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+      if (
+        scrollTop + clientHeight >= scrollHeight - 1 &&
+        !isLoading &&
+        data &&
+        page < data.totalPages
+      ) {
+        console.log("Reached to Bottom!");
+        setPage((prev) => prev + 1);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [data, isLoading]);
 
   return (
     <section className="products_list_section">
@@ -42,30 +68,30 @@ const ProductsList = () => {
       </header>
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {isLoading
-          ? skeletons.map((n) => <ProductCardSkeleton key={n} />)
-          : data?.products &&
-            data.products.map((product) => (
-              <ProductCard
-                key={product._id}
-                id={product._id}
-                image={product.images[0]}
-                price={product.price}
-                title={product.title}
-                rating={product.reviews.rate}
-                ratingCounts={product.reviews.count}
-                stock={product.stock}
-              />
-            ))}
+
+        {data?.products &&
+          data.products.map((product) => (
+            <ProductCard
+              key={product._id}
+              id={product._id}
+              image={product.images[0]}
+              price={product.price}
+              title={product.title}
+              rating={product.reviews.rate}
+              ratingCounts={product.reviews.count}
+              stock={product.stock}
+            />
+          ))}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
       </div>
-      {data && (
+      {/* {data && (
         <Pagination
           totalPosts={data?.totalProducts}
           postsPerPage={8}
           onClick={handlePageChange}
           currentPage={page}
         />
-      )}
+      )} */}
     </section>
   );
 };
