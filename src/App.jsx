63644cb5454501076004dbb272bcf0217ff1e208
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-02-29 19:39:08 -0500

    Getting user from Token

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const jwtUser = jwtDecode(jwt);
+      if (Date.now() >= jwtUser.exp * 1000) {
+        localStorage.removeItem("token");
+        location.reload();
+        setUser(null);
+      } else {
+        setUser(jwtUser);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <div className="app">
       <Navbar />
