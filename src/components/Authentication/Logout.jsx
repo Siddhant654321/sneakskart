eef882d218a6a2519f5536367e6d90183154453a
Siddhant <technosys.sidd@gmail.com>
2024-03-01 17:43:50 -0500

    Implementing Logout functionality

---
+++ src/components/Authentication/Logout.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+const Logout = () => {
+  useEffect(() => {
+    localStorage.removeItem("token");
+    window.location = "/";
+  }, []);
+  return null;
+};
+export default Logout;
