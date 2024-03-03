8abcb60ae962c015187c3becdee73635578a95e9
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-03-03 17:00:12 -0500

    Add to Cart in Product Card

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -71,16 +71,7 @@ const ProductsList = () => {
 
         {data?.products &&
           data.products.map((product) => (
-            <ProductCard
-              key={product._id}
-              id={product._id}
-              image={product.images[0]}
-              price={product.price}
-              title={product.title}
-              rating={product.reviews.rate}
-              ratingCounts={product.reviews.count}
-              stock={product.stock}
-            />
+            <ProductCard key={product._id} product={product} />
           ))}
         {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
       </div>
