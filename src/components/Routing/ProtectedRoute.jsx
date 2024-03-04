10f098f95157a2c727d757957f9064769c6ca390
Siddhant <technosys.sidd@gmail.com>
2024-03-03 20:19:43 -0500

    Fetching Featured Products

--- src/components/Routing/ProtectedRoute.jsx
+++ src/components/Routing/ProtectedRoute.jsx
@@ -1,18 +1,15 @@
+import React from "react";
 import { getUser } from "../../services/userServices";
-import { Outlet, Navigate, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-  return getUser() ? (
-    <Outlet />
-  ) : (
-    <Navigate
-      to="/login"
-      state={{
-        from: location.pathname,
-      }}
-    />
-  );
+    return getUser() ? (
+        <Outlet />
+    ) : (
+        <Navigate to='/login' state={{ from: location.pathname }} />
+    );
 };
+
 export default ProtectedRoute;
