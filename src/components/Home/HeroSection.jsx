ad1d134d536ea24bd9b6e6cd020fa52a658381e6
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-03-03 20:19:43 -0500

    Fetching Featured Products

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,4 +1,5 @@
 import "./HeroSection.css";
+import { Link } from "react-router-dom";
 
 const HeroSection = ({ title, subtitle, link, image }) => {
   return (
@@ -6,14 +7,16 @@ const HeroSection = ({ title, subtitle, link, image }) => {
       <div className="align_center">
         <h2 className="hero_title">{title}</h2>
         <p className="hero_subtitle">{subtitle}</p>
-        <a href="" className={link}>
+        <Link to={link} className="hero_link">
           Buy Now
-        </a>
+        </Link>
       </div>
+
       <div className="align_center">
         <img src={image} alt="" className="hero_image" />
       </div>
     </section>
   );
 };
+
 export default HeroSection;
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,6 +1,8 @@
-import HeroSection from "./HeroSection";
-import iPhone from "../../assets/iphone-14-pro.webp";
+import React from "react";
+
+import iphone from "../../assets/iphone-14-pro.webp";
 import mac from "../../assets/mac-system-cut.jfif";
+import HeroSection from "./HeroSection";
 import FeaturedProducts from "./FeaturedProducts";
 
 const HomePage = () => {
@@ -9,20 +11,20 @@ const HomePage = () => {
       <HeroSection
         title="Buy iPhone 14 Pro"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
-        link="/"
-        image={iPhone}
+        link="/product/65de99c52228644db406ee97"
+        image={iphone}
       />
+
       <FeaturedProducts />
 
       <HeroSection
         title="Build the ultimate setup"
-        subtitle="You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini."
-        link="/"
+        subtitle="You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac book pro."
+        link="/product/65de99c52228644db406ee9f"
         image={mac}
       />
-
-      {/* Hero Section */}
     </div>
   );
 };
+
 export default HomePage;
