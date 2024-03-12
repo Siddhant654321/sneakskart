be0d3f58648158440082d430555b724e01346690
Siddhant <technosys.sidd@gmail.com>
2024-03-12 13:23:52 +0530

    added the search bar and suggestions in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./Navbar.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate, Link } from "react-router-dom";
 import UserContext from "../../contexts/UserContext";
 import CartContext from "../../contexts/CartContext";
 import { getSuggestionsAPI } from "../../services/productServices";
 
 const Navbar = () => {
+
+  const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(-1);
+
+  const navigate = useNavigate();
 
   const user = useContext(UserContext);
   const { cart } = useContext(CartContext);
@@ -20,12 +26,93 @@ const Navbar = () => {
     e.stopPropagation();
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search.trim() !== "") {
+      navigate(`/products?search=${search.trim()}`);
+    }
+    setSuggestions([]);
+  };
+
+  const handleKeyDown = (e) => {
+    if (selectedItem < suggestions.length) {
+      if (e.key === "ArrowDown") {
+        setSelectedItem((current) =>
+          current === suggestions.length - 1 ? 0 : current + 1
+        );
+      } else if (e.key === "ArrowUp") {
+        setSelectedItem((current) =>
+          current === 0 ? suggestions.length - 1 : current - 1
+        );
+      } else if (e.key === "Enter" && selectedItem > -1) {
+        const suggestion = suggestions[selectedItem];
+        navigate(`/products?search=${suggestion.title}`);
+        setSearch("");
+        setSuggestions([]);
+      }
+    } else {
+      setSelectedItem(-1);
+    }
+  };
+
+  useEffect(() => {
+    const delaySuggestions = setTimeout(() => {
+        if (search.trim() !== "") {
+            getSuggestionsAPI(search)
+                .then((res) => setSuggestions(res.data))
+                .catch((err) => console.log(err));
+        } else {
+            setSuggestions([]);
+        }
+    }, 300);
+
+    return () => clearTimeout(delaySuggestions);
+  }, [search]);
+
   return (
     <header id="navbar">
       <nav className="navbar-container container">
         <NavLink href="/" className="home-link">
           WishCart
         </NavLink>
+        <form className="align_center navbar_form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="navbar_search"
+            placeholder="Search Products"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button type="submit" className="search_button">
+            Search
+          </button>
+
+          {suggestions.length > 0 && (
+            <ul className="search_result">
+              {suggestions.map((suggestion, index) => (
+                <li
+                  className={
+                    selectedItem === index
+                      ? "search_suggestion_link active"
+                      : "search_suggestion_link"
+                  }
+                  key={suggestion._id}
+                >
+                  <Link
+                    to={`/products?search=${suggestion.title}`}
+                    onClick={() => {
+                      setSearch("");
+                      setSuggestions([]);
+                    }}
+                  >
+                    {suggestion.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </form>
         <button
           type="button"
           id="navbar-toggle"
@@ -44,32 +131,50 @@ const Navbar = () => {
           onClick={toggleNavbarVisibility}
         >
           <ul className="navbar-links" onClick={handleNavbarLinksClick}>
-            <li className="navbar-item"><NavLink className="navbar-link" to="/">Home</NavLink></li>
-            <li className="navbar-item"><NavLink className="navbar-link" to="/products">Products</NavLink></li>
+            <li className="navbar-item">
+              <NavLink className="navbar-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink className="navbar-link" to="/products">
+                Products
+              </NavLink>
+            </li>
             {!user && (
-                <>
-                    <li className="navbar-item"><NavLink className="navbar-link" to="/login">LogIn</NavLink></li>
-                    <li className="navbar-item"><NavLink className="navbar-link" to="/signup">SignUp</NavLink></li>
-                </>
+              <>
+                <li className="navbar-item">
+                  <NavLink className="navbar-link" to="/login">
+                    LogIn
+                  </NavLink>
+                </li>
+                <li className="navbar-item">
+                  <NavLink className="navbar-link" to="/signup">
+                    SignUp
+                  </NavLink>
+                </li>
+              </>
             )}
             {user && (
-                    <>
-                        <li className="navbar-item">
-                            <NavLink className="navbar-link" to="/myorders">My Orders</NavLink>
-                        </li>
-                        <li className="navbar-item">
-                            <NavLink className="navbar-link" to="/logout">Logout</NavLink>
-                        </li>
-                        <li className="navbar-item">
-                            <NavLink to='/cart' className='navbar-link'>
-                                Cart{" "}
-                                <p className='align_center cart_counts'>
-                                    {cart.length}
-                                </p>
-                            </NavLink>
-                        </li>
-                    </>
-                )}
+              <>
+                <li className="navbar-item">
+                  <NavLink className="navbar-link" to="/myorders">
+                    My Orders
+                  </NavLink>
+                </li>
+                <li className="navbar-item">
+                  <NavLink className="navbar-link" to="/logout">
+                    Logout
+                  </NavLink>
+                </li>
+                <li className="navbar-item">
+                  <NavLink to="/cart" className="navbar-link">
+                    Cart{" "}
+                    <p className="align_center cart_counts">{cart.length}</p>
+                  </NavLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </nav>
