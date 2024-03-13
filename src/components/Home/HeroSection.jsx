9334b8694e961e10b00e82070ce968ce33e623e0
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-03-13 14:18:08 +0530

    added all the spheres along with aligning them in various places and also improved the main image in the hero section

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -8,6 +8,15 @@ const HeroSection = () => {
   return (
     <section className="hero_section">
       <img src={sneakers_hero_section} alt='sneakers' className="main_hero_image" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_1 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_2 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_3 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_4 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_5 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_6 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_7 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_8 hero_section_sphere" />
+      <img src={sphere_hero_section} alt="" className="hero_section_sphere_9 hero_section_sphere" />
     </section>
   );
 };
