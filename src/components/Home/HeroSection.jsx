3d980c988fcbae11ed3643a7c020b0d2e9dc6a69
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-03-13 12:55:18 +0530

    added the hero section sneakers image in the center of the hero section

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -7,7 +7,7 @@ import sphere_hero_section from '../../assets/sphere_hero_section.png'
 const HeroSection = () => {
   return (
     <section className="hero_section">
-
+      <img src={sneakers_hero_section} alt='sneakers' className="main_hero_image" />
     </section>
   );
 };
