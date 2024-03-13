11e6f48631aa9f5876ad4bfa2efd1afaa11b6bd6
Sasnarine Deodat <sasnarinedeodat407@gmail.com>
2024-03-13 12:49:32 +0530

    added the sneakers hero section image and the sphere image for the hero section

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "./HeroSection.css";
+import sneakers_hero_section from '../../assets/sneakers_hero_section.png'
+import sphere_hero_section from '../../assets/sphere_hero_section.png'
 
 const HeroSection = () => {
   return (
